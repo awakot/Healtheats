0b4c1a8b5c39febdb6ed300eb6437bf9fb751792
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-17 18:07:44 +0900

    Installed jquery-rails and react-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
