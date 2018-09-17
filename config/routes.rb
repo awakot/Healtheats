6f976158217d86ed7556815d9c5ce5207b21e119
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-17 18:22:18 +0900

    Generated menus controller and api/menus controller api/menus.json returns constant json

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'menus/index'
+  end
+  namespace :api do
+    resources :menus, only: :index
+  end
+  get 'menus/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
