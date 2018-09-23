1e18bebc1f0b7d55932c012e3082d3cb7f318c81
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-23 23:56:43 +0900

    /api/menus.json に対するspecを追加

--- spec/views/menus_spec.rb
+++ spec/views/menus_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "menus/index", :type => :view do
+RSpec.describe "menus/index", type: :view do
   it "shows sample strings" do
     # TODO: Reactをテストする
     render
