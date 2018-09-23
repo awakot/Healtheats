2f793bb04dc52aebadf0ae239358b3638e6be56e
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-23 23:47:53 +0900

    menus/index の view spec(mock)を追加

---
+++ spec/views/menus_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "menus/index", :type => :view do
+  it "shows sample strings" do
+    # TODO: Reactをテストする
+    render
+    expect(rendered).to have_css("p", text: "ここにReactコンポーネント")
+  end
+end
