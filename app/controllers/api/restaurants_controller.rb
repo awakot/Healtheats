c856e4403019684621c62078bc31a1dfe7482492
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-29 16:41:41 +0900

    Modified indent

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class Api::RestaurantsController < ApplicationController
   def index
-      # /api/restaurants.json で仮のJSONを返す
+    # /api/restaurants.json で仮のJSONを返す
     # TODO: DBから値取得
     @data = [
       { "name": "サパテロ", "zip_code": 1500002, "address": "ほげほげ" },
