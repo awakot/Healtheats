c487229d8f42d066ab8f701b2f0c1dbc41689cb8
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-30 00:02:39 +0900

    userのログイン・ログアウト処理のテスト（モック）を追加

--- spec/features/user_login_and_logout_spec.rb
+++ spec/features/user_login_and_logout_spec.rb
@@ -1,6 +1,18 @@
 RSpec.describe "login/logout path", type: :helper do
-  it "enables user to successfully log in and log out" do
+  let!(:user) { FactoryBot.create(:user) }
+
+  it "enables user to successfully log in" do
     get login_path
-    
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
+    find_button('Login').click
+    current_path.should == root_path # ログインフォームのpost先を指定
+    expect(rendered).to have_css("p", "ログインしました") # <= あくまで一例なので変えてね
+  end
+
+  it "enables user to successfully log out" do
+    post logout_path, params: { email: user.email }
+    current_path.should == root_path # ログアウトフォームのpost先を指定
+    expect(rendered).to have_css("p", "ログアウトしました") # <= あくまで一例なので変えてね
   end
 end
