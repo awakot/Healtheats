725af4ea2e4c5e3380fce28e1969877a9bd2d380
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-30 23:14:36 +0900

    specファイルの微修正

--- spec/views/menus/index.html.erb_spec.rb
+++ spec/views/menus/index.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "menus/index", type: :view do
-  it "shows sample strings" do
+  it "shows menus list" do
     render
     expect(rendered).to have_css("h1", text: "メニュー一覧")
     # TODO: Reactをテストしたいけど無理そう ><
