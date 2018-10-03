e5d797fd61d46df295ececa81666346425bbf08f
waytkheming <kawamura.estyle@gmail.com>
2018-10-03 13:25:41 +0900

    bugFix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
 
 # 渡された文字列のハッシュ値を返す
-  def digest(string)
+  def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                 BCrypt::Engine.cost
                                                 BCrypt::Password.create(string, cost: cost)
