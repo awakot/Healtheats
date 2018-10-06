9c997104cb0e5ec11adc0d61b9bcac515118be0f
ayumitamai97 <ayumitamai97@gmail.com>
2018-10-06 23:25:34 +0900

    Modified migration files

---
+++ db/migrate/20181006121927_create_menus.rb
@@ -0,0 +1,16 @@
+class CreateMenus < ActiveRecord::Migration[5.2]
+  def change
+    create_table :menus do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :price, null: false
+      t.string :picture, null: false
+      t.references :calorie, foreign_key: true
+      t.references :restaurant, foreign_key: true
+      t.references :category, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
