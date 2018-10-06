42f093edb78283092185047bf0f650f71f933a2c
waytkheming <kawamura.estyle@gmail.com>
2018-10-06 15:49:19 +0900

    bugFix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,9 +13,10 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: 'カテゴリーが更新されました。' }
-      enlse
+      else
         format.html { render :edit}
       end
+    end
   end
 
   def index
@@ -27,6 +28,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: 'カテゴリーが削除されました。' }
+    end
   end
 
 private
