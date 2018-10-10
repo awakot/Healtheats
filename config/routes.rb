09dc953a5a7d51107af50391cce452a9f413e687
To Kawa <33716911+waytkheming@users.noreply.github.com>
2018-10-11 01:38:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :categories
+  resources :tags
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
