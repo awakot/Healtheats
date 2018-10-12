a842c96ddaf6d324fb9c224bbddb72798808892a
ayumitamai97 <ayumitamai97@gmail.com>
2018-10-12 14:09:35 +0900

    menus_controllerのspecを追加(renderingの成功のみをテスト)

--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "restaurants/index", type: :view do
+RSpec.describe "menus/index", type: :view do
 
   describe "GET #index" do
     it "successfully renders template" do
