70977fd1691105e5c9f0b2e8bc8aeba135c67e75
ayumitamai97 <ayumitamai97@gmail.com>
2018-10-22 03:27:52 +0900

    Added hamburger menu styles and js

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,11 @@
+function openMenu(){
+  $(".navbar").css("display", "block");
+  $(".openMenuBtn").css("display", "none");
+  $(".closeMenuBtn").css("display", "block");
+};
+
+function closeMenu(){
+  $(".navbar").css("display", "none");
+  $(".openMenuBtn").css("display", "block");
+  $(".closeMenuBtn").css("display", "none");
+};
