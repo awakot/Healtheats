be14ca9d40dc00ba77d9f7532f6a78df7820027c
ayumitamai97 <ayumitamai97@gmail.com>
2018-10-27 01:16:47 +0900

    1カテゴリあたり3つのメニューをランダムに表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    categories = Category.all
+    @json = 
+      categories
+      .preload(:menus)
+      .map { |category| category.as_json.merge( { menus: category.menus.shuffle.take(3).as_json }) }
   end
 end
