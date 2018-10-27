d8f3cd1f6cdabfbc06fbd2063ad483c5cae86933
ayumitamai97 <ayumitamai97@gmail.com>
2018-10-27 14:52:17 +0900

    Revert finding menus based on location

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,14 +8,21 @@ class CategoriesController < ApplicationController
       categories
       .preload(:menus)
       .map { |category| category.as_json.merge({ 
-        menus: category.menus
-               .map{ |menu|
-                 restaurant = Restaurant.find(menu.restaurant_id)
-                 lat = restaurant.latitude
-                 lon = restaurant.longitude
-                 restaurant_location = [lat, lon]
-                 return menu if Geocoder::Calculations.distance_between(current_location, restaurant_location) <= 1
-               }.shuffle.take(3).as_json
+        menus: category.menus.shuffle.take(3).as_json
       })}
+
+    # @json = 
+    #   categories
+    #   .preload(:menus)
+    #   .map { |category| category.as_json.merge({ 
+    #     menus: category.menus
+    #            .map{ |menu|
+    #              restaurant = Restaurant.find(menu.restaurant_id)
+    #              lat = restaurant.latitude
+    #              lon = restaurant.longitude
+    #              restaurant_location = [lat, lon]
+    #              return menu if Geocoder::Calculations.distance_between(current_location, restaurant_location) <= 1
+    #            }.shuffle.take(3).as_json
+    #   })}
   end
 end
